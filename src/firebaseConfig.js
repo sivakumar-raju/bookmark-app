77d916aa9b95ce5f864bf0331a03386fc6fbf32c
sivakumar-raju <52912495+sivakumar-raju@users.noreply.github.com>
2019-12-17 11:57:53 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,5 @@
 export default {
-    apiKey: "AIzaSyAZzUQ2yfd3jltDhuWqhJbR56wigI-hoNE",
-    authDomain: "bookmark-bc065.firebaseapp.com",
-    databaseURL: "https://bookmark-bc065.firebaseio.com",
-    projectId: "bookmark-bc065",
-    storageBucket: "bookmark-bc065.appspot.com",
-    messagingSenderId: "918226635187",
+    
+    // Add your firebase config details here
+    
 };
